b366cd39da555f24c3209c0a30bb02faa389a73f
Leevi Kerkelä <leevi.kerkela@protonmail.com>
2022-10-13 10:13:39 +0200

    Added a link to the source

--- data/test-subject/fit_smt.sh
+++ data/test-subject/fit_smt.sh
@@ -1,3 +1,5 @@
+# fitmcmicro is available at https://github.com/ekaden/smt
+
 ./fitmcmicro --bvals dwi.bval --bvecs dwi.bvec test_signals.nii.gz smt_results.nii.gz
 
 ./fitmcmicro --mask brain_mask.nii.gz --bvals dwi.bval --bvecs dwi.bvec dwi.nii.gz smt_maps.nii.gz
