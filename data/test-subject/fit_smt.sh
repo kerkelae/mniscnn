de4ebda5a9471d40e61ac0b7e9a5cbf770f7765e
Leevi Kerkelä <leevi.kerkela@protonmail.com>
2022-10-13 10:11:30 +0200

    NLLS fit

---
+++ data/test-subject/fit_smt.sh
@@ -0,0 +1,3 @@
+./fitmcmicro --bvals dwi.bval --bvecs dwi.bvec test_signals.nii.gz smt_results.nii.gz
+
+./fitmcmicro --mask brain_mask.nii.gz --bvals dwi.bval --bvecs dwi.bvec dwi.nii.gz smt_maps.nii.gz
